09d5dff9bd087e86428c44097760aa700915e9d4
Alex Yanai <yanai.alex@gmail.com>
2016-10-20 00:47:13 -0400

    adds replicator module

---
+++ lib/cloudant/replicator.rb
@@ -0,0 +1,11 @@
+module Cloudant
+  module Replicator
+    # The Security Module contains methods to read and modify existing users,
+    # permissions, and credentials. 
+    # The default credentials provided upon account creaton have _admin level
+    # access to all account databases; any subsequent users or API keys created
+    # must have permissions explicitly set.
+    #
+
+  end
+end
---
+++ spec/replicator_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Cloudant::Replicator' do
+  before do
+    @cloudant = Cloudant::Client.new({username: 'test', password: 'test', database: 'test'})
+  end
+
+  context '' do
+    it '' do
+    end
+  end
+end
