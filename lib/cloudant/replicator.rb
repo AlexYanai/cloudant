a8c529b0bdd5a1ac192ecb0b7470fc1e18de8628
Alex Yanai <yanai.alex@gmail.com>
2016-10-20 23:47:30 -0400

    updates replicate method

--- lib/cloudant/replicator.rb
+++ lib/cloudant/replicator.rb
@@ -1,9 +1,22 @@
 module Cloudant
   module Replicator
     # The Replicator Module contains methods to replicate a database
-    # 
-    def active_tasks
-      @conn.query({url_path: "_active_tasks", method: :get})
+    #
+    # Allows you to monitor a replication 
+    def active_tasks(type="replication")
+      @conn.query({url_path: "_active_tasks", opts: {"type" => type}, method: :get})
+    end
+
+    def replicate_db(target)
+      doc = {
+        "source": "https://#{username}:#{password}@#{username}.cloudant.com/#{database}",
+        "target": "https://#{username}:#{password}@#{username}.cloudant.com/#{target}",
+        "create_target": true,
+        "continuous": true
+      }
+      doc_name = Cloudant::Utility.generate_doc_name(database,target)
+
+      @conn.query({url_path: "_replicator/#{doc_name}", opts: doc, method: :put})
     end
   end
 end
